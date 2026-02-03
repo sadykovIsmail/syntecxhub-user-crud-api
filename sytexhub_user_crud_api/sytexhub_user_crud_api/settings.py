84670476a1e61f1e040be0745f134d13fc3f59ae
Ismail <ghvh8176@gmail.com>
2026-02-03 00:31:02 -0600

    connect to postgres

--- sytexhub_user_crud_api/sytexhub_user_crud_api/settings.py
+++ sytexhub_user_crud_api/sytexhub_user_crud_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +78,15 @@ WSGI_APPLICATION = 'sytexhub_user_crud_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drf_project',
+        'USER': 'postgres',
+        'PASSWORD': 'Ismail2006.,',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
