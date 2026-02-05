97388093bf84b2f7623817b5484453c7009f69e7
Ismail <ghvh8176@gmail.com>
2026-02-05 10:07:29 -0600

    create user

--- sytexhub_user_crud_api/users/models.py
+++ sytexhub_user_crud_api/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.username
